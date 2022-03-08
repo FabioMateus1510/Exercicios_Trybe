69e543040bf981386ef6a408f9d7558abcffc4f9
FabioMateus1510 <fabiomateus1510@gmail.com>
2022-03-08 11:37:32 -0300

    positivo, negativo e zero

---
+++ Fundamentos/B4_Intro_JavaScript_Logica_de_Programacao/Dia01_Primeiros_Passos/Ex_04.js
@@ -0,0 +1,13 @@
+//4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+const a = 2022;
+
+if(a > 0){
+    console.log('positivo');
+}
+else if (a < 0){
+    console.log('negative');
+}
+else{
+    console.log('zero');
+}
