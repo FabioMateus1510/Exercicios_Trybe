08bee6433667e8d21496f20b3e0c4befae92029e
FabioMateus1510 <fabiomateus1510@gmail.com>
2022-03-08 10:57:20 -0300

    exercicao maior dos 2

---
+++ Fundamentos/B4_Intro_JavaScript_Logica_de_Programacao/Dia01_Primeiros_Passos/Ex_02.js
@@ -0,0 +1,11 @@
+//2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const a = 100;
+const b = 50;
+
+if(a > b){
+    console.log("maior numero a" + "(" + a + ")");
+}
+else{
+    console.log("maior numero b" + "(" + b + ")"); 
+}
