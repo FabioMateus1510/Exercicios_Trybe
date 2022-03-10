f6b7ec5b2694d2cc9eef744ba5f343e4d6589a63
FabioMateus1510 <fabiomateus1510@gmail.com>
2022-03-10 08:26:12 -0300

    Dividindo por 2

---
+++ Fundamentos/B4_Intro_JavaScript_Logica_de_Programacao/02_Array_e_For/Ex_09.js
@@ -0,0 +1,6 @@
+//09.Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+let numeros = [0];
+for(let i = 1; i <= 25; i++){
+numeros.push(i); //numeros[i]= i; 
+console.log(numeros[i] / 2);
+}
