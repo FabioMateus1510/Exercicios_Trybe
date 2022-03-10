dcdfeeba9c0d0680b393d2db627f6930c1530b6f
FabioMateus1510 <fabiomateus1510@gmail.com>
2022-03-10 07:52:08 -0300

    Encontrando o maior valor

--- Fundamentos/B4_Intro_JavaScript_Logica_de_Programacao/02_Array_e_For/Ex_05.js
+++ Fundamentos/B4_Intro_JavaScript_Logica_de_Programacao/02_Array_e_For/Ex_05.js
@@ -1,6 +1,6 @@
 //05.Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maior = 0;
+let maior;
 
 for (let numero of numbers) {
     if(numero > maior){
